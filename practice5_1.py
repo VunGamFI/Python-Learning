52943bcf0c42f95835cb4c0c964d1714bc84930b
糊掉的麵 <jaqueswin@gmail.com>
2023-11-03 10:29:57 +0800

    Add files via upload

---
+++ practice5_1.py
@@ -0,0 +1,32 @@
+print("Is 30 x 5 = 150? I predict True.")
+print(30 * 5 == 150)
+
+print("Is 4 > 9? I predict False.")
+print(4 > 9)
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("Is car == 'Subaru'? I predict False.")
+print(car == 'Subaru')
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+print("Is 'falafel' in my_foods? I predict True.")
+print('falafel' in my_foods)
+
+print("Is 'dumpling' in my_foods? I predict False.")
+print('dumpling' in my_foods)
+
+print("Is 'pizza' and 'dumpling' in my_foods? I predict False.")
+print('pizza' and 'dumpling' in my_foods)
+
+print("Is 'pizza' or 'dumpling' in my_foods? I predict True.")
+print(('pizza' in my_foods) or ('dumpling' in my_foods))
+
+print("Is 9 > 10? I predict False.")
+print(9 > 10)
+
+abc = 'aBc'
+print("Is ABC.lower() = 'abc'? I predict True")
+print(abc.lower() == 'abc')
