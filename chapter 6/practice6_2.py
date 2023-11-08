117910639a42a07df50324a58c91abfb65f3661c
糊掉的麵 <jaqueswin@gmail.com>
2023-11-08 10:45:42 +0800

    Add files via upload

---
+++ chapter 6/practice6_2.py
@@ -0,0 +1,13 @@
+favorite_nums = {
+    'Mike' : '2',
+    'Bob' : '15',
+    'Marry' : '23',
+    'Jack' : '9',
+    'Tim' : '0',
+    }
+
+print(f"Mike喜欢的数字是{favorite_nums['Mike']}")
+print(f"Bob喜欢的数字是{favorite_nums['Bob']}")
+print(f"Marry喜欢的数字是{favorite_nums['Marry']}")
+print(f"Jack喜欢的数字是{favorite_nums['Jack']}")
+print(f"Tim喜欢的数字是{favorite_nums['Tim']}")
---
+++ chapter 6/practice6_3.py
@@ -0,0 +1,13 @@
+dictionary_0 = {
+    'method' : '方法，是Python可对数据执行的操作',
+    'comment' : '注释',
+    'append' : '追加',
+    'pop' : '弹出',
+    'list comprehension' : '列表推导式',
+    }
+
+print(f"method : {dictionary_0['method']}")
+print(f"comment : {dictionary_0['comment']}")
+
+print(f"pop\n\n{dictionary_0['pop']}\n")
+print(f"list comprehension\n\n{dictionary_0['list comprehension']}")
